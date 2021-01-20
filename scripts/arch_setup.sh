183d1bfdc6c4b33322c6b970eb44c486b549d4a5
Quinton <dropatuningmetal@gmail.com>
2021-01-20 01:34:08 -0700

    vanilla arch revamp

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -3,64 +3,65 @@
 bold="\e[1;36m"
 close="\e[0m"
 
-# install the san francisco fonts
-function inssanfran {
-yay -S otf-san-francisco
-}
+git_dir=~/Git
+
+if [ ! -d $git_dir ]; then
+	mkdir $git_dir
+fi
 
 # install the mac cursor to .icons
 function inscursor {
 cd ~/Downloads || exit
 git clone https://github.com/douglascomim/MacOSMOD.git
 
-if [ ! -d ~/.icons ]; then
-mkdir ~/.icons
-fi
-
-mv ~/Downloads/MacOSMOD/ ~/.icons/
+sudo mv ~/Downloads/MacOSMOD/ /usr/share/icons/
 cd || exit
 }
 
-# install zsh plugins (zsh autosuggestions and zsh syntax highlighting
-function zshcustom {
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo -e "Dont forget to add:\n${bold}zsh-syntax-highlighting zsh-autosuggestions${close}\nto zshrc"
+# Install YAY for vanilla arch
+function insyay {
+	if ! [ -x "$(command -v yay)" ]; then
+		pacman -S --needed git base-devel
+		git clone https://aur.archlinux.org/yay.git $git_dir
+		cd yay || exit
+		makepkg -si
+	fi
 }
 
-function insmenlo {
-yay -S ttf-menlo-powerline-git
+
+function wal_persist {
+	echo "wal -R" >> ~/.xinitrc
 }
 
 # full arch-based linux setup
 function yayinstall {
 # apt apps that need to be installed
-AppList=(picom otf-san-francisco ttf-menlo-powerline-git python-pywal dconf-editor grub-customizer rofi dunst tlp tree neofetch neovim zsh git curl powertop wget xfce4-terminal alacritty variety visual-studio-code-bin remmina gpick polybar)
+AppList=(lxappearance nitrogen i3-gaps picom blueman bpytop firefox lightdm lightdm-gtk-greeter-settings lightdm-gtk-greeter noto-fonts noto-fonts-emoji openssh pavucontrol pulseaudio pulseaudio-bluetooth qogir-icon-theme-git reflector spotify sshfs stow sudo base-devel python-pywal dconf-editor grub-customizer rofi dunst tlp tree neofetch neovim zsh git curl powertop wget xfce4-terminal alacritty variety visual-studio-code-bin remmina gpick polybar pcmanfm timeshift thunderbird zsh-autosuggestions zsh-syntax-highlighting pamac-aur xfce4-power-manager clipit)
 
-for app in "${AppList[@]}"
-do
-echo "Installing ${app}"
-yay -S $app
-done
-
-# change terminal colors with gogh
-# bash -c  "$(wget -qO- https://git.io/vQgMr)" 
-
-# install oh my zsh
-if [ ! -d ~/.oh-my-zsh ]; then
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+yay -S --needed ${AppList[*]}
 }
 
-function help {
-    echo ""
-    statements=("Yay Setup Commands:" "${bold}inscursor${close} -- Installs macosx cursor theme" "${bold}insmenlo${close} -- installs menlo font" "${bold}yayinstall${close} -- Installs full arch setup" "${bold}zshcustom${close} -- Install all the zsh autosuggestion and syntax highlighting" "${bold}inssanfran${close} -- Install the San Francisco font")
-    for items in "${statements[@]}"
-    do
-    echo -e $items
-    done
-    echo ""
+function gogh {
+
+	# change terminal colors with gogh
+	bash -c  "$(wget -qO- https://git.io/vQgMr)"
+
 }
 
-"$@"
+
+
+# Alright, now that all the functions are made, lets get to the install script
+
+# Install Yay to start
+insyay
+
+# Install all apps
+yayinstall
+
+# ins the cursor
+inscursor
+
+# Append to the end of .xinitrc for pywal color persistance
+wal_persist
+
+
