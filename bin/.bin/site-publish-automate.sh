260dcf622d771996f61e08694bffa4309412711d
Quinton Jasper <qjj2@nau.edu>
2023-02-17 13:03:25 -0700

    switched modkeys: MOD=ALT and SUP=WinKey

---
+++ bin/.bin/site-publish-automate.sh
@@ -0,0 +1,21 @@
+#!/usr/bash
+
+# Arguments
+projpath="$1"
+#TODO username="$2"
+#TODO pass="$3"
+
+# Variables
+#TODO serverURL=""
+
+
+###  navigate to project folder ###
+# If projpath is directory
+  # navigate to project directory
+## Else
+  # exit 1
+
+### Connect to the remote server via SMB ###
+remote_connect () {
+  #TODO
+}
