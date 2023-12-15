6ca3290dfbe3a248b1e7c823172c2b40909ffea4
Quinton Jasper <dropatuningmetal@gmail.com>
2023-12-14 21:06:44 -0700

    diffed lilbuddy and main configs to confirm each works

--- configuration.nix
+++ configuration.nix
@@ -8,18 +8,14 @@
   imports =
     [ # Include the results of the hardware scan.
       #./hardware-configuration.nix
-      /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
   # Bootloader.
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "BLACKBOX-NIX"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   services.xserver = {
     enable = true;
@@ -103,23 +99,20 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.quinton = {
     isNormalUser = true;
     description = "Quinton Jasper";
-    extraGroups = [ "networkmanager" "wheel" "video" "kvm"];
+    extraGroups = [ "networkmanager" "wheel" "video" "kvm" "libvirtd" ];
     packages = with pkgs; [
       vivaldi
       vivaldi-ffmpeg-codecs
       sublime-merge
       remmina
+      gh
     ];
   };
 
@@ -145,23 +138,12 @@
   kitty
   xdg-desktop-portal-gtk
   rofi-wayland
-  libgcc
   pcmanfm
   networkmanager-openconnect
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -169,12 +151,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
   # add flake features
