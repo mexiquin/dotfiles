c20e9423eea8ee7c77a71e7669db61a464a7aec5
Quinton Jasper <dropatuningmetal@gmail.com>
2026-01-22 22:50:30 -0700

    testing nix-hardware config for macbook 11,4

--- modules/home-manager/theming-qt.nix
+++ modules/home-manager/theming-qt.nix
@@ -3,6 +3,13 @@
 {
   qt = {
     enable = true;
+    platformTheme.name = "gtk";
     style.name = "adw-gtk3-dark";
+    qt6ctSettings = {
+      Appearance = {
+        style = "adw-gtk3-dark";
+        icon-theme = "Tela-dark";
+      };
+    };
   };
 }
