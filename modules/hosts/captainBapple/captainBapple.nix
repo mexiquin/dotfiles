44a78256f40c398226c244201310eeed958aad2e
Quinton Jasper <dropatuningmetal@gmail.com>
2026-01-24 00:03:33 -0700

    set 2x sddm scaling on CaptainBapple only

--- modules/hosts/captainBapple/captainBapple.nix
+++ modules/hosts/captainBapple/captainBapple.nix
@@ -9,4 +9,10 @@
   # Set hostname
   networking.hostName = "CaptainBapple";
 
+  # Scale SDDM for Retina display
+  services.displayManager.sddm.settings = {
+    General = {
+      GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2";
+    };
+  };
 }
