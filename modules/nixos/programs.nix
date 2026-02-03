f36dbe667602050e86fde8d620a804823776e35f
Quinton Jasper <dropatuningmetal@gmail.com>
2026-02-02 19:15:05 -0700

    add usb automnt support

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -1,4 +1,7 @@
 { ... }:
 {
-  # Placeholder module - neovim config moved to home-manager
+ # let pcmanfm/file browsers auto mount drives
+ services.gvfs.enable = true;
+ services.udisks2.enable = true;
+ services.devmon.enable = true;
 }
