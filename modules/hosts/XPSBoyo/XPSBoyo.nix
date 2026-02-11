d78899f3a96318c969f877a73e988653e46cb495
Quinton Jasper <dropatuningmetal@gmail.com>
2026-02-10 18:05:52 -0700

    added ollama to XPSBOYO

--- modules/hosts/XPSBoyo/XPSBoyo.nix
+++ modules/hosts/XPSBoyo/XPSBoyo.nix
@@ -6,10 +6,19 @@
     ./hardware-configuration.nix
     ../../nixos/hardware/nvidia-hybrid.nix
   ];
-
+  
   networking.hostName = "XPSBoyo"; # Define your hostname.
-
+  
   # List packages installed in system profile for this host only.
   environment.systemPackages = with pkgs; [
   ];
+  
+  # enable similar to blackbox configuration
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-cuda;
+    environmentVariables = {
+      OLLAMA_KEEP_ALIVE = "-1";
+    };
+  };
 }
